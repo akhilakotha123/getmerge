de3bf5accdbdfdde0c777a12d2b28fc31849e92b
git881 <kothaakhila123@gmail.com>
2020-12-18 16:06:53 +0530

    first commit

---
+++ DemoApp/src/com/lti/model/Area.java
@@ -0,0 +1,8 @@
+package com.lti.model;
+
+public class Area {
+	public void area()
+	{
+		System.out.println("Area");
+	}
+}
---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Calc {
+	public void add(int a,int b)
+	{
+		System.out.println("Sum="+(a+b));
+	}
+	public void diff(int a,int b)
+	{
+		System.out.println("Differenece="+(a-b));
+	}
+	
+}
